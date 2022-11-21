d762f9266ee7bfc2b8e912fca061bab1aa7deea3
alekswheeler <alexskntks@outlook.com>
2022-11-21 12:59:41 -0300

    feat:loginWithToken

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { verifyUserData } from '../middlewares/verifyUserData'
+import { CreateLoginController } from '../Users/useCases/createLogin/CreateLoginController'
 import { CreateUserController } from '../Users/useCases/createUser/CreateUserController'
 
 const createUserController = new CreateUserController()
-
+const createLoginController = new CreateLoginController()
 const usersRoutes = Router()
 
 usersRoutes.post('/', verifyUserData, createUserController.handle)
+usersRoutes.post('/login', createLoginController.handle)
 
 export { usersRoutes }
